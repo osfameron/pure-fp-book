10fe037f99e5b87bf594fc0749420bf2301f3153
Hakim Cassimally <hakim@eschew.local>
2011-06-13 23:43:02 +0100

    map code

--- code/perl/lib/List.pm
+++ code/perl/lib/List.pm
@@ -9,6 +9,7 @@ role List {
     requires 'head';
     requires 'tail';
 
+    # can be called as a class method
     method fromArray ($self: @array) {
         if (! @array) {
             return List::Empty->new;
@@ -20,6 +21,7 @@ role List {
             tail => $self->fromArray(@tail),
         );
     }
+
     multi method nth (List::Empty $self: Int $pos) { 
         die "Can't index into an Empty list"; 
     }
@@ -29,17 +31,29 @@ role List {
     multi method nth (Int $pos where { $_ > 0 }) {
         tail $self->tail->nth( $pos - 1 );
     }
+
+    multi method map (List::Link $self: CodeRef $f) {
+        tail $self->new( 
+            head => $f->($self->head),
+            tail => $self->tail->map( $f )
+        );
+    }
+    multi method map (List::Empty $self: CodeRef $f) { 
+        return $self;
+    }
 }
 
 class List::Link with List {
+    use Sub::Call::Tail;
+
     has head => ( is => 'ro', isa => 'Any' );
     has tail => ( is => 'ro', isa => 'List' ),
+
 }
 
 class List::Empty with List {
     method head { die "Can't take head of empty list!" }
     method tail { die "Can't take head of empty list!" }
-
 }
 
 1;
--- code/perl/t/chapter_01/01_list.t
+++ code/perl/t/chapter_01/01_list.t
@@ -1,6 +1,9 @@
+use strict; use warnings;
 use Test::More;
 use Test::Moose;
 use Test::Exception;
+
+use signatures;
 
 use_ok ('List');
 
@@ -9,14 +12,34 @@ my $list = List->fromArray( 10..20 );
 does_ok  ($list, 'List' );
 isa_ok   ($list, 'List::Link' );
 
-is $list->head,    10, 'First value ok';
-is $list->nth(1),  11, 'This one goes to 11';
-is $list->nth(10), 20, 'Tenth element ok';
+is $list->head,       10, 'First value ok';
+is $list->nth(1),     11, 'This one goes to 11';
+is $list->tail->head, 11, '... and this one';
+is $list->nth(10),    20, 'Tenth element ok';
 dies_ok {
     $list->nth(11);
 } 'exception on >10';
 dies_ok {
     $list->nth(-1);
 } 'exception on <0';
+
+my @users = ({
+    first_name => 'Bob',
+    last_name  => 'Smith',
+}, {
+    first_name => 'Aisha',
+    last_name  => 'Chaudhury',
+});
+sub full_name ($record) {
+    return join ' ' => 
+        $record->{first_name},
+        $record->{last_name};
+}
+
+my $users = List->fromArray(@users);
+my $names = $users->map( \&full_name );
+
+is $names->nth(0), 'Bob Smith',       'map ok';
+is $names->nth(1), 'Aisha Chaudhury', 'map ok';
 
 done_testing;
