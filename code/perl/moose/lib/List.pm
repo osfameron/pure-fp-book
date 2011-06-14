81511e6fd81903547b53efbedd660f2ba24e7ae1
Hakim Cassimally <hakim@eschew.local>
2011-06-15 00:32:06 +0100

    note on multi methods.

--- code/perl/moose/lib/List.pm
+++ code/perl/moose/lib/List.pm
@@ -82,7 +82,7 @@ role List {
 
 class List::Link with List {
     has head => ( is => 'ro', isa => 'Any' );
-    has tail => ( is => 'ro', isa => 'List' ),
+    has tail => ( is => 'ro', isa => 'List' ); 
 }
 
 class List::Empty with List {
